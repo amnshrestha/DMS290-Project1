c550b4c1ab307f161a85208b1900f098076a0c99
bhall10 <78971176+bhall10@users.noreply.github.com>
2021-03-13 22:32:23 -0500

    Spelling error for "Francisco"
    Fixed spelling error. Also deleted footer.php that was in the wrong location

--- inc/footer.php
+++ inc/footer.php
@@ -1,4 +1,4 @@
  <footer>
-    <div>Source: <a href= "http://bmart13.digitalscholar.rochester.edu/assignment03/">San Fransico</a></div>
+    <div>Source: <a href= "http://bmart13.digitalscholar.rochester.edu/assignment03/">San Francisco</a></div>
     <div>Winnie Wan - Bianca Hall - Duncan Grubbs for DMS 290</div>
   </footer>
--- inc/nav.php
+++ inc/nav.php
@@ -5,7 +5,7 @@
         <li><a href="yangzhou.php">yangzhou</a></li>
         <li><a href="fuzhou.php">fuzhou</a></li>
         <li><a href="giza.php">giza</a></li>
-        <li><a href="sf.php">san fransico</a></li>
+        <li><a href="sf.php">san francisco</a></li>
     </ul>
   </nav>
 </header>
